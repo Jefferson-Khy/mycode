2a9eefab4fbac3543ea2dbef64b63c75fdce1ebd
Jeff Khy <jeffersonk@tlgcohort.com>
2024-08-12 21:24:28 +0000

    day One python

---
+++ challenge19.py
@@ -0,0 +1,10 @@
+def main():
+    """
+        asks user for input for their name and then 
+        day of the week and print it back to the user
+    """
+    user_input1 = input("what is your name?: ")
+    user_input2 = input("what day of the week is it?: ")
+    print(f"Hello, {user_input1}! Happy {user_input2}!")
+
+main()
