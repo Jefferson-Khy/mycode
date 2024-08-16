61c976c997fc52770b1c6ffe8aa7ea3841047a46
Jeff Khy <jeffersonk@tlgcohort.com>
2024-08-16 20:12:12 +0000

    nasa api challenge

---
+++ nasaApiChallenge.py
@@ -0,0 +1,9 @@
+https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-06-15
+
+
+https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=3
+
+https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2024-08-12&end_date=2024-08-16
+
+
+
